b621a0a8a1ad592ba5edc8af623f9c341f720484
Fabrizio Recchia <fabrizio643@hotmail.it>
2024-07-28 21:05:12 +0200

    quasi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,17 +7,16 @@ function Home() {
   return (
     <>
       <Navbar />
-
       <div className="text-center">
         <img src={MedPos} alt="Med-pos" />
       </div>
       <div className="text-center">
-        <button type="button" className="btn btn-primary m-2 mb-5 px-5 py-2">
+        <button type="button" className="btn btn-primary mb-5 px-5 py-2">
           <Link className="link" to={`/Blog`}>
             Blog
           </Link>
         </button>
-        <button type="button" className="btn btn-info m-2 mb-5 px-5 py-2">
+        <button type="button" className="btn btn-info mb-5 px-5 py-2">
           <Link className="link" to={`/Timer`}>
             Timer
           </Link>
